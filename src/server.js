eea94e5b284cfcd562a9624a012c42bdae647bc8
Johnny.Seubert <johnny.seubert@partners.mv.com.br>
2022-08-07 21:13:14 -0300

    process.env

--- src/server.js
+++ src/server.js
@@ -3,14 +3,11 @@ const routes = require('./routes')
 const db = require('./database')
 
 const server = express()
+const port = process.env.PORT || 3000
 
 server.use(express.json())
 server.use('/api', routes)
 
-db.sync()
-   .then(() => console.log("Banco conectado com sucesso!"))
+db.sync().then(() => console.log("Banco conectado com sucesso!"))
 
-
-server.listen(3333, () => {
-   console.log("Servidor rodando");
-})
+server.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
